e2d17c7328d1c432e22f334a001f8486862e4cdc
Marek <wojnarmarek0@gmail.com>
2022-03-21 23:18:40 +0100

    local and global time fix

--- src/com/company/Main5.java
+++ src/com/company/Main5.java
@@ -1,14 +1,18 @@
 package com.company;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.time.LocalTime;
+
+import java.util.Date;
 
 public class Main5 {
 
     public static void main(String[] args) {
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println("local date " + dtf.format(now));
+        LocalTime localTime = LocalTime.now();
+        System.out.println("local date " + localTime);
+
+        Date globalTime = new Date();
+        System.out.println("global date " + globalTime);
     }
 }
