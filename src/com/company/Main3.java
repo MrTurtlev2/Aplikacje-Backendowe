53a65b5b91f1625bbd44960cb0b08fe4de01a754
Marek <wojnarmarek0@gmail.com>
2022-03-22 22:51:28 +0100

    main 3 almost done

--- src/com/company/Main3.java
+++ src/com/company/Main3.java
@@ -1,21 +1,72 @@
 package com.company;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
+import static com.company.Main3.choseMiddle;
 
-//public class Main3 {
-//
-////    public static void main(String[] args) {
-//
-//        private static splitArray(int[] array, int value, int index, int limit) {
-//            if (index > limit) {
-//                return -1;
+public class Main3 {
+
+    public static int result;
+
+    public static int choseMiddle(int[] array, int value) {
+
+
+        int index = 0;
+        int length = array.length - 1;
+
+        while (index <= length) {
+            int point = (int) Math.ceil((index + length) / 2);
+            int entry = array[point];
+
+            if(value > entry) {
+                index = point + 1;
+            }
+            if (value < entry) {
+                length = point -1;
+            }
+        }
+        System.out.println(length);
+        result = length;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[]{4, 5, 7, 11, 12, 15, 15, 21, 40, 45};
+        int value = 11;
+
+            choseMiddle(myIntArray, value);
+
+            System.out.println(result);
+    }
+
+}
+
+
+
+
+
+//    function searchIndex(array, value) {
+//        var index = 0;
+//        var limit = array.length - 1;
+//        while (index <= limit) {
+//            var point = Math.ceil((index + limit) / 2);
+//            var entry = array[point];
+//            if (value > entry) {
+//                index = point + 1;
+//                continue;
 //            }
-//            int point = (int) Math.ceil(())
+//            if (value < entry) {
+//                limit = point - 1;
+//                continue;
+//            }
+//            return point; // value == entry
 //        }
+//        return -1;
+//    }
 //
 //
-////    }
-//}
+//// Usage example:
+//
+//    // for this implementation array must be sorted from smallest to biggest!
+//    var array = [ 4, 5, 7, 11, 12, 15, 15, 21, 40, 45 ];
+//        var index = searchIndex(array, 11); // we want to find index for 11
+//
+//        console.log(index);  // 3
---
+++ src/com/company/Main7.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+
+public class Main7 {
+
+    public static void main(String[] args) {
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        User userObject = new User("John", 21);
+//        String userJson = objectMapper.writeValueAsString(userObject);
+//
+//        System.out.println(userJson);
+    }
+}
+//
+//class User {
+//
+//    private String name;
+//    private int age;
+//
+//    public User(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//}
